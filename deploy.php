a2ab9b8afaa61dbf14fc0fc425352a58c658cd3c
Marcio Vennan <marciovennan@gmail.com>
2016-09-11 16:48:11 -0300

    alterando configuracoes de deploy

--- deploy.php
+++ deploy.php
@@ -7,7 +7,7 @@ server('producao', '191.101.9.10', 22)
 	->user('root')
 	->password('Nf(fv4{Ms75Av]b')
 	->stage('producao')
-    ->env('deploy_path', '/var/www/html/fiscalizacao_transito');
+    ->env('deploy_path', '/var/www/html/livraria_online');
 
-set('repository', 'https://github.com/marciovennan/teste_ci.git');
+set('repository', 'https://github.com/marciovennan/livraria.git');
 
