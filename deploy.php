6571514792289da530c88fe81732d581eb3dc964
Marcio Vennan <marciovennan@gmail.com>
2016-09-14 12:26:56 -0300

    teste deploy

--- deploy.php
+++ deploy.php
@@ -11,8 +11,10 @@ server('producao', '191.101.9.10', 22)
 
 set('repository', 'https://github.com/marciovennan/livraria.git');
 
-task('run', 
-	array( 
-		'deploy:prepare', 
-		'deploy:update_code', 
-	))->desc('Deploy para servidor');
+task('deploy:renomeacao_arquivos', function () {
+    run("cd /var/www/html/livraria_online/current");
+    run("v .htaccess.dist .htaccess");
+    run("cd application/config && mv config.php.dist config.php");
+    run("cd tests && mv bootstrap.php.dist bootstrap.php");
+
+})->desc('Renomeando arquivos');
