a58dfbaf35f992625cb27373df24c7c5cbd5aed8
Marcio Vennan <marciovennan@gmail.com>
2016-09-18 19:50:04 -0300

    corrigindo caminho errado para upload de imagens

--- application/models/livromodel.php
+++ application/models/livromodel.php
@@ -45,7 +45,7 @@ class LivroModel
         if (!empty($_FILES['imagem']['name']) && $_FILES['imagem']['name'] != '') {
             $livro['imagem'] =  $_FILES['imagem']['name'];
             //var_dump($_FILES['imagem']); die;
-            $upload = upload::factory('livraria' . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'img');
+            $upload = upload::factory('livraria_online' . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'img');
             $upload->file($_FILES['imagem']);
             $upload->set_filename($_FILES['imagem']['name']);
 
@@ -104,7 +104,7 @@ class LivroModel
             $query->bindValue(':imagem', $livro['imagem']);
         }
              //var_dump($_FILES['imagem']); die;
-            $upload = upload::factory('livraria' . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'img');
+            $upload = upload::factory('livraria_online' . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'img');
             $upload->file($_FILES['imagem']);
             $upload->set_filename($_FILES['imagem']['name']);
 
