365113e42d49a2bfb0c130617d253b8c46071905
Marcio Vennan <marciovennan@gmail.com>
2016-09-18 21:26:39 -0300

    retirando menu sem utlidade paa a aplicacao

--- application/views/_templates/header-admin.php
+++ application/views/_templates/header-admin.php
@@ -71,14 +71,14 @@
                         </li>
                         </ul>
                     </li>
-                    <li class="dropdown">
+  <!--                   <li class="dropdown">
                         <a href=""  class="dropdown-toggle" data-toggle="dropdown">Reservas<b class="caret"></b></a>
                         <ul class="dropdown-menu">
                         <li>
                             <a href="<?=URL?>reservas/indexAdmin">Listar</a>
                         </li>
                         </ul>
-                    </li>
+                    </li> -->
                     <li class="dropdown">
                         <a href=""  class="dropdown-toggle" data-toggle="dropdown">Categorias<b class="caret"></b></a>
                         <ul class="dropdown-menu">
