d3ba898413aaa6bb0728acbd3d4983d4550a9003
Marcio Vennan <marciovennan@gmail.com>
2016-09-30 11:55:14 -0300

    criado metodo que calcula model e um teste unitario para o mesmo

--- application/models/aluguelmodel.php
+++ application/models/aluguelmodel.php
@@ -171,26 +171,54 @@ class AluguelModel
    * @param String @data_devolucao        data que realmente o livro foi entregue
    * @return float
    */
-  private function calculaMulta(string $data_prevista_entrega, string $data_devolucao)
-  { 
-        $valor_multa_por_dia = 1.0;
-        $total_multa = 0.0;
+
+  // private function calculaMulta(string $data_prevista_entrega, string $data_devolucao) { 
+  //       $valor_multa_por_dia = 1.0;
+  //       $total_multa = 0.0;
 
         /**
          * A função strtotime retorna o timestamp de cada data, ou seja, o numero de segundo
          * desde 1970. Calcula a diferênça entre elas, o que segnifica o número de segundos que 
-         * o livro está em atraso. 
+         * o livro está em atraso.
          */ 
-        $segundo_em_atraso = strtotime($data_prevista_entrega) - strtotime($data_devolucao);
+  //       $segundo_em_atraso = strtotime($data_prevista_entrega) - strtotime($data_devolucao);
+
+  //       // Transforma os segundos em dias
+  //       $dias = $segundo_em_atraso / (60 * 60 * 24);
+
+  //       // calcula o valor da multa
+  //       $total_multa = $valor_multa_por_dia * $dias;
+
+  //       return $total_multa;
+  // }
+
+
+  public function calculaMulta(string $data_prevista_entrega, string $data_devolucao) { 
+        
+        // variável que armazena o valor da multa cobrado por dia
+        $md = 1.0;
+
+        // variável que armazenará o total da multa calculada
+        $tm = 0.0;
+
+        /**
+         * A função strtotime retorna o timestamp de cada data, ou seja, o numero de segundo
+         * desde 1970. Calcula a diferênça entre elas, o que segnifica o número de segundos que 
+         * o livro está em atraso.
+         */ 
+        $segundo_em_atraso = strtotime($data_devolucao) - strtotime($data_prevista_entrega);
 
         // Transforma os segundos em dias
         $dias = $segundo_em_atraso / (60 * 60 * 24);
 
         // calcula o valor da multa
-        $total_multa = $valor_multa_por_dia * $dias;
+        $tm = $md * $dias;
 
-        return $total_multa;
+        return $tm;
   }
+
+
+
 
 
   function add($reserva_id, $cliente_id, $livro_id, $preco_aluguel)
