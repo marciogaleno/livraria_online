f150b0c25ecdd645c462a4c09eb87616b9bca8cf
Marcio Vennan <marciovennan@gmail.com>
2016-10-05 22:20:37 -0300

    criado metodo para calcularMulta por atraso e seu teste unitario

--- application/models/aluguelmodel.php
+++ application/models/aluguelmodel.php
@@ -163,32 +163,32 @@ class AluguelModel
    }
 
 
-  public function calculaMulta(string $data_prevista_entrega, string $data_devolucao) { 
-        
-    // variável que armazena o valor da multa cobrado por dia
-    $md = 1.0;
+    public function calculaMulta(string $data_prevista_entrega, string $data_devolucao) {
+          
+        // variável que armazena o valor da multa cobrado por dia
+        $md = 1.0;
 
-    // variável que armazenará o total da multa calculada
-    $tm = 0.0;
+        // variável que armazenará o total da multa calculada
+        $tm = 0.0;
 
-    if (!empty($data_prevista_entrega) && !empty($data_devolucao)){
-      /**
-       * A função strtotime retorna o timestamp de cada data, ou seja, o numero de segundo
-       * desde 1970. Calcula a diferênça entre elas, o que segnifica o número de segundos que 
-       * o livro está em atraso.
-       */ 
-      $segundo_em_atraso = strtotime($data_devolucao) - strtotime($data_prevista_entrega);
+        if (!empty($data_prevista_entrega) && !empty($data_devolucao)){ 
+          /**
+           * A função strtotime retorna o timestamp de cada data, ou seja, o numero de segundo
+           * desde 1970. Calcula a diferênça entre elas, o que segnifica o número de segundos que
+           * o livro está em atraso.
+           */ 
+          $segundo_em_atraso = strtotime($data_devolucao) - strtotime($data_prevista_entrega);
 
-      // Transforma os segundos em dias
-      $dias = $segundo_em_atraso / (60 * 60 * 24);
+          // Transforma os segundos em dias
+          $dias = $segundo_em_atraso / (60 * 60 * 24);
 
-      // calcula o valor da multa
-      $tm = $md * $dias;     
+          // calcula o valor da multa
+          $tm = $md * $dias;     
+        }
+
+
+         return $tm;
     }
-
-
-    return $tm;
-  }
 
 
 
