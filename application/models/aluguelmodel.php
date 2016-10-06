bab71c89f414fede9146f64ed18769233c0b7eed
Marcio Vennan <marciovennan@gmail.com>
2016-10-05 22:32:39 -0300

    criado metodo para calcularMulta por atraso e seu teste unitario

--- application/models/aluguelmodel.php
+++ application/models/aluguelmodel.php
@@ -171,19 +171,19 @@ class AluguelModel
         // variável que armazenará o total da multa calculada
         $tm = 0.0;
 
-        if (!empty($data_prevista_entrega) && !empty($data_devolucao)){ 
+        if (!empty($data_prevista_entrega) && !empty($data_devolucao)){
           /**
            * A função strtotime retorna o timestamp de cada data, ou seja, o numero de segundo
            * desde 1970. Calcula a diferênça entre elas, o que segnifica o número de segundos que
            * o livro está em atraso.
-           */ 
-            $segundo_em_atraso = strtotime($data_devolucao) - strtotime($data_prevista_entrega);
+           */
+            $segundo_em_atraso = strtotime($data_devolucao) - strtotime($data_prevista_entrega); 
 
             // Transforma os segundos em dias
             $dias = $segundo_em_atraso / (60 * 60 * 24);
 
             // calcula o valor da multa
-            $tm = $md * $dias;     
+            $tm = $md * $dias;
         }
 
 
