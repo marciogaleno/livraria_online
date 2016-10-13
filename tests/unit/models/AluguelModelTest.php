5c34331472ad6ea02e5367592615c62f86297916
Dev A <desenvolvedor_a@el0.com.br>
2016-10-13 15:50:48 -0300

    criado pipeline bitbucket

--- tests/unit/models/AluguelModelTest.php
+++ tests/unit/models/AluguelModelTest.php
@@ -34,9 +34,13 @@ class AluguelModelTest extends PHPUnit
 		$data_prevista_entrega = '07-09-2016';
 		$data_devolução = '10-09-2016';
 
+		$dias_em_atraso = 3;
+		$valor_multa_dia = 1.5;
+		$total_multa = $dias_em_atraso * $valor_multa_dia;
+
  		// valor da multa por dia é R$ 1.50, então no caso das datas de exemplo que simula 3 
  		// dias de atraso o método calculaMulta deve retornar o valor 4.5.
-		$this->assertEquals(4.5, $aluguelModel->calculaMulta($data_prevista_entrega, $data_devolução));
+		$this->assertEquals($total_multa, $aluguelModel->calculaMulta($data_prevista_entrega, $data_devolução));
 		
 	}
 
